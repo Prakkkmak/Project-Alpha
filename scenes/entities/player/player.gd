489755b4558a5bfcdf133d0c2988b5f2814e1934
Lévy <prakkmak@gmail.com>
2024-04-13 19:21:14 +0200

    Systeme de dialogue avec Dialogic

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -1,1 +1,8 @@
 extends CharacterBody2D
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("dialog_open"):
+		Dialogic.start('first_timeline')
+		get_viewport().set_input_as_handled()
+		
