265d8c09e4603cf50b0acd5319b81fabae59d6c4
Lévy <prakkmak@gmail.com>
2024-04-15 19:01:39 +0200

    Fix texture changement

--- scenes/entities/npc/npc.gd
+++ scenes/entities/npc/npc.gd
@@ -7,28 +7,24 @@ extends CharacterBody2D
 @onready var interactable_component: InteractableComponent = $InteractableComponent
 
 func _ready() -> void:
+	# Initialisation de la texture du sprite
 	sprite.texture = data.idle_texture
-	duplicate_animation_libraries()
-	setup_animations()
+	# Connecter les signaux d'animation
+	connect_animation_signals()
 	setup_interactable_component()
 
-func duplicate_animation_libraries() -> void:
-	var libraries: Array[StringName] = animation_player.get_animation_library_list()
-	for i: int in range(libraries.size()):
-		var library_name: String = libraries[i]
-		var library: AnimationLibrary = animation_player.get_animation_library(library_name).duplicate(true)
-		var new_library_name: String = "{0}_{1}".format(library_name, str(i))  # Création d'un nom unique
-		animation_player.remove_animation_library(library_name)
-		animation_player.add_animation_library(new_library_name, library)
+func connect_animation_signals() -> void:
+	# Connecter le signal 'animation_changed' à une méthode de gestion
+	animation_player.animation_changed.connect(_on_AnimationPlayer_animation_changed)
 
-func setup_animations() -> void:
-	var anim_list: PackedStringArray = animation_player.get_animation_list()
-	for anim_name: String in anim_list:
-		var anim: Animation = animation_player.get_animation(anim_name)
-		if anim:
-			var track_no: int = anim.find_track("Sprite2D:texture", Animation.TYPE_VALUE)
-			anim.track_insert_key(track_no, 0, data.idle_texture if "idle" in anim_name else data.walk_texture)
+func _on_AnimationPlayer_animation_changed(old_name: String, new_name: String) -> void:
+	# Changer la texture du sprite en fonction du nom de l'animation jouée
+	if "idle" in new_name:
+		sprite.texture = data.idle_texture
+	elif "walk" in new_name:
+		sprite.texture = data.walk_texture
 
 func setup_interactable_component() -> void:
+	# Configuration spécifique si l'interaction est de type TalkInteraction
 	if interactable_component.interaction is TalkInteraction:
 		(interactable_component.interaction as TalkInteraction).npc_data = data
