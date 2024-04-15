e779cc9486e63a36e1237bc805e1a0a3c48d8087
Lévy <prakkmak@gmail.com>
2024-04-15 07:49:48 +0200

    Add NpcData file

--- scenes/entities/npc/npc.gd
+++ scenes/entities/npc/npc.gd
@@ -1,17 +1,16 @@
 extends CharacterBody2D
 
 
-@export var idle_texture: Texture
-@export var walk_texture: Texture
+@export var data: NpcData
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
 
 
 func _ready() -> void:
-	sprite.texture = idle_texture
+	sprite.texture = data.idle_texture
 	for anim_name: String in animation_player.get_animation_list():
 		var anim: Animation = animation_player.get_animation(anim_name)
 		if anim:
 			var track_no: int = anim.find_track("Sprite2D:texture", Animation.TYPE_VALUE)
-			anim.track_insert_key(track_no, 0, idle_texture if "idle" in anim_name else walk_texture)
+			anim.track_insert_key(track_no, 0, data.idle_texture if "idle" in anim_name else data.walk_texture)
---
+++ scripts/npc_data.gd
@@ -0,0 +1,14 @@
+class_name NpcData extends Resource
+
+@export_category("Textures")
+@export var idle_texture: Texture
+@export var walk_texture: Texture
+
+@export_category("Timelines")
+@export var greetings_timeline: DialogicTimeline
+@export var talk_timeline: DialogicTimeline
+@export var help_timeline: DialogicTimeline
+
+@export_category("Help Conditions")
+@export var true_conditions: Array[String]
+@export var false_conditions: Array[String]
