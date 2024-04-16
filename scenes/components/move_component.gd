9671a4e3e76c93e71ca27601746ee7f08c2aeafd
Lévy <prakkmak@gmail.com>
2024-04-16 14:05:13 +0200

    Frozen npc

--- scenes/components/move_component.gd
+++ scenes/components/move_component.gd
@@ -5,7 +5,7 @@ class_name MoveComponent extends Node
 @export var _max_speed: float = 70
 @export var _acceleration: float = 20
 @export var _character_body: CharacterBody2D
-
+@export var _frozen: bool = false
 
 var direction_string_v: String = "down"
 var direction_string_h: String = "right"
@@ -17,6 +17,8 @@ var _direction: Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	if !_character_body:
+		return
+	if _frozen:
 		return
 	_velocity = _velocity.lerp(_direction * _max_speed, 1 - exp(-_acceleration * delta))
 	_character_body.velocity = _velocity
