1429d3039680a3b2a76d47c0c7a6d93c45c0a840
Lévy <prakkmak@gmail.com>
2024-04-17 11:52:22 +0200

    remove object on text

--- scripts/interactions/event_interaction.gd
+++ scripts/interactions/event_interaction.gd
@@ -2,12 +2,37 @@ class_name EventInteraction extends Interaction
 
 @export var event_name: String
 @export var timeline: DialogicTimeline
+@export var resolve_conditions: Array[TimelineCondition]
+@export var delete_on_resolve: bool = true
+
+var target_area: Area2D
 
 func perform_interaction(source: Area2D, target: Area2D) -> void:
-	Dialogic.start(timeline)
-	Dialogic.timeline_ended.connect(_on_timeline_ended, ConnectFlags.CONNECT_ONE_SHOT)
-	
+	target_area = target
+	if GameState.get_state(event_name):
+		# Vérifie et lance immédiatement la timeline si les conditions sont remplies
+		if not _launch_resolved_timeline():
+			# Si aucune condition n'est remplie, rejouer la timeline de base
+			Dialogic.start(timeline)
+			Dialogic.timeline_ended.connect(_on_timeline_ended, ConnectFlags.CONNECT_ONE_SHOT)
+	else:
+		# Lancer la timeline de base pour la première fois
+		Dialogic.start(timeline)
+		Dialogic.timeline_ended.connect(_on_timeline_ended, ConnectFlags.CONNECT_ONE_SHOT)
 
 func _on_timeline_ended() -> void:
 	GameState.set_state(event_name)
+	ended.emit()
+
+func _launch_resolved_timeline() -> bool:
+	for condition: TimelineCondition in resolve_conditions:
+		if condition.are_conditions_met():
+			Dialogic.start(condition.timeline)
+			Dialogic.timeline_ended.connect(_on_resolve_event, ConnectFlags.CONNECT_ONE_SHOT)
+			return true  # Retourne vrai dès qu'une timeline est lancée
+	return false  # Retourne faux si aucune condition n'est remplie
+
+func _on_resolve_event() -> void:
+	if delete_on_resolve and target_area and target_area.owner is Node2D:
+		(target_area.owner as Node2D).queue_free()
 	ended.emit()
